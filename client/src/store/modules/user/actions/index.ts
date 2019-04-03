2c54f60f8b6102b2d9361bde7eca11e53f28b2bc
Alexander Selivanov <alexander.v.selivanov@gmail.com>
2019-04-03 17:25:08 +0300

    upgrade typescript version to 3.4.1

--- client/src/store/modules/user/actions/index.ts
+++ client/src/store/modules/user/actions/index.ts
@@ -4,5 +4,5 @@ import {ApplicationError, Login, Token} from 'template-common';
 import {loginCreator, getUserCreator, getUserByIdCreator} from './types';
 
 export const login = loginCreator<Login, Token, ApplicationError>();
-export const getUser = getUserCreator<void, User, ApplicationError>();
+export const getUser = getUserCreator<undefined, User, ApplicationError>();
 export const getUserById = getUserByIdCreator<Id>();
--- client/src/store/root/actions/index.ts
+++ client/src/store/root/actions/index.ts
@@ -2,5 +2,5 @@ import {ApplicationError} from 'template-common';
 
 import {initializeCompleteCreator, initializeFailCreator} from './types';
 
-export const initializeComplete = initializeCompleteCreator<void>();
+export const initializeComplete = initializeCompleteCreator<undefined>();
 export const initializeFail = initializeFailCreator<ApplicationError>();
