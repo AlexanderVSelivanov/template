1570f66c7d36985f0936ab5eef675c12e37f4b07
Alexander.V.Selivanov <Alexander.V.Selivanov@gmail.com>
2019-05-29 04:13:20 +0300

    add openstreet map

--- client/src/modules/map/view/pages/main/page.tsx
+++ client/src/modules/map/view/pages/main/page.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Map, Marker, Popup, TileLayer, Viewport} from 'react-leaflet';
 
 import useStyles from './styles';
 
 type PageProps = {};
 
+const DEFAULT_VIEWPORT: Viewport = {
+  center: [40.7484, 73.9857],
+  zoom: 10,
+};
+
 const Page: React.FC<PageProps> = () => {
   const classes = useStyles();
+  const [viewport, setViewport] = useState<Viewport>(DEFAULT_VIEWPORT);
+
+  const onClickReset = () => {
+    setViewport(DEFAULT_VIEWPORT);
+  };
+
+  const onViewportChanged = (viewport: Viewport) => {
+    setViewport(viewport);
+  };
   return (
-    <>
-      Map
-    </>
+    <Map
+      onClick={onClickReset}
+      onViewportChanged={onViewportChanged}
+      viewport={viewport}
+    >
+      <TileLayer
+        attribution="&amp;copy <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </Map>
   );
 };
 
