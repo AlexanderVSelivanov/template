a1591eac43c04167cc1c9251ce41c0da8ccb92e3
Alexander Selivanov <Alexander.V.Selivanov@gmail.com>
2019-06-16 17:52:23 +0300

    update user account

--- server/src/modules/user/cotrollers/putUserById.ts
+++ server/src/modules/user/cotrollers/putUserById.ts
@@ -2,13 +2,36 @@ import {Id, UserDto} from 'template-common';
 import userRepositoryFactory from '../../../store/repository/userRepository';
 import {badRequest, Controller, notFound, ok} from '../../../utils/ControllerBuilder';
 import userToUserDto from '../../../types/mappers/userToUserDto';
+import AccountEntity from '../../../store/entities/AccountEntity';
+import accountRepositoryFactory from '../../../store/repository/accountRepository';
 
 const putUserByIdController: Controller<UserDto & { id: Id }, UserDto> = async ({input}) => {
   if (input && input.id) {
     const userRepository = userRepositoryFactory();
-    const user = await userRepository.findOne(input.id);
+    const user = await userRepository.findOne(input.id, {relations: ['account']});
     if (user) {
-      const updatedUser = await userRepository.save({...user, ...input, id: user.id});
+      user.firstName = input.firstName;
+      user.lastName = input.lastName;
+      user.email = input.email;
+      if (input.account) {
+        const account = user.account ? user.account : new AccountEntity();
+        account.username = input.account.username;
+        account.disable = Boolean(input.disable);
+        if (input.account.password) {
+          const password = AccountEntity.getPasswordHash(input.account.password);
+          account.passwordHash = password.hash;
+          account.passwordSalt = password.salt;
+        }
+        const accountRepository = accountRepositoryFactory();
+        user.account = await accountRepository.save(account);
+      } else {
+        if (user.account) {
+          const accountRepository = accountRepositoryFactory();
+          await accountRepository.remove(user.account);
+        }
+        user.account = undefined;
+      }
+      const updatedUser = await userRepository.save(user);
       return ok(userToUserDto(updatedUser));
     }
     return notFound('User entity not found');
